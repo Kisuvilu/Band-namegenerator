b596254982c1c42fdaf91674938ddb9e51e42617
Dennis Musembi @kisuvilu <71897350+Kisuvilu@users.noreply.github.com>
2023-05-02 11:22:42 +0300

    initial commit

---
+++ Band name generator.py
@@ -0,0 +1,10 @@
+#create a greeting for the program
+print("welcome to band name generator.")
+#Ask the user for the city that tey grew up in.
+city = input("which city did you grow up in?\n")
+#Ask the user for the name of the pet.
+pet = input("what is the name of a pet?\n")
+#combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " +pet)
+
+
